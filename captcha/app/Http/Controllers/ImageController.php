8e3148f3465a579ba84d7b761a0fb30566f256a0
luca <lucabrugnera01@gmail.com>
2023-05-22 09:40:33 +0200

    Move ImageServices into ImageController

--- captcha/app/Http/Controllers/ImageController.php
+++ captcha/app/Http/Controllers/ImageController.php
@@ -63,4 +63,31 @@ class ImageController extends Controller
     {
         //
     }
+
+    private function getAvailableClasses() : array {
+        $array = Image::select('class')->distinct()->get()->toArray();
+        foreach($array as $key => $value){
+            $array[$key] = $value['class'];
+        }
+        return $array;
+    }
+
+    public function getCaptchaClasses(int $num_of_classes) : array{
+        $array = $this->getAvailableClasses();
+        $captcha_classes = [];
+        for($i = 0; $i<$num_of_classes; $i++){
+            $rnd = rand(0,count($array)-1);
+            $captcha_classes[$i] = $array[$rnd];
+            array_splice($array, $rnd, 1);
+        }
+        return $captcha_classes;
+    }
+
+    public function updateReliability(string $id, int $reliability) : void {
+        Image::where('id', $id)->update(['reliability' => $reliability]);
+    }
+
+    public function getImage (string $class, int $reliability): Image {
+        return Image::where('class', $class)->where('reliability', $reliability)->inRandomOrder()->first();
+    }
 }
