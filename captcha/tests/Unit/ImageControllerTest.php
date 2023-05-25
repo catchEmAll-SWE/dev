f81ca3408bac5ce3415ae00aaf9ecb6a259dc1bc
luca <lucabrugnera01@gmail.com>
2023-05-25 11:56:41 +0200

    fix test error

--- captcha/tests/Unit/ImageControllerTest.php
+++ captcha/tests/Unit/ImageControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Http\Controllers\API\V1\ImageController;
+use App\Models\Reliability;
 use InvalidArgumentException;
 use OutOfBoundsException;
 use Tests\TestCase;
@@ -36,6 +37,6 @@ class ImageControllerTest extends TestCase
     public function test_get_images_of_undefined_class() : void
     {
         $this->expectException(InvalidArgumentException::class);
-        $this->controller->getImagesIdOfClass('undefined', 1);
+        $this->controller->getImagesOfClass('undefined', 1, Reliability::Reliable);
     }
 }
