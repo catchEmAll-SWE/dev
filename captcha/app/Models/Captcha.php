1308c64178b4b6e27a8108eae6efd8cce40bdec4
luca <lucabrugnera01@gmail.com>
2023-05-26 11:37:18 +0200

    Add captcha model

---
+++ captcha/app/Models/Captcha.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Http\Business\CaptchaImg;
+use App\Http\Business\CaptchaImgBuilder;
+use App\Http\Business\ProofOfWorkDetails;
+
+class Captcha {
+    private CaptchaImg $captcha_img;
+    private ProofOfWorkDetails $proof_of_work_details;
+
+    public function __construct()
+    {
+        $this->captcha_img = CaptchaImgBuilder::getGenerator()->getCaptchaImg();
+        $this->proof_of_work_details = new ProofOfWorkDetails($this->captcha_img->getId());
+    }
+
+    public function getCaptchaImg() : CaptchaImg {
+        return $this->captcha_img;
+    }
+
+    public function getProofOfWorkDetails() : ProofOfWorkDetails {
+        return $this->proof_of_work_details;
+    }
+}
