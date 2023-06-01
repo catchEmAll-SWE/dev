442e3d8e279ba72bed880fb3e585767fcc897d18
luca <lucabrugnera01@gmail.com>
2023-06-01 10:04:53 +0200

    Remove unnecessary code comments

--- captcha/tests/Feature/CaptchaControllerTest.php
+++ captcha/tests/Feature/CaptchaControllerTest.php
@@ -4,25 +4,11 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use App\Models\User;
-use Illuminate\Support\Collection;
 use Laravel\Sanctum\Sanctum;
 use Illuminate\Testing\Fluent\AssertableJson;
 
 class CaptchaControllerTest extends TestCase
 {
-    /*
-    Test generate:
-        - Request unauthorized (401)
-        - Request authorized (200), con json avente come attributi:
-            - captchaImg
-                - src 
-                - solution
-                - keyNumber
-            - proofOfWorkDetails
-                - fixedString
-                - difficulty 
-    */
-
     public function test_generate_captcha_route_without_token () : void {
         $response = $this->get('api/v1/generate');
         $response->assertRedirect('docs');
@@ -54,18 +40,6 @@ class CaptchaControllerTest extends TestCase
             )
         );
     }
-
-
-    /*
-    Test verify:
-        - Request unauthorized (401)
-        - Request authorized (200), con attributi:
-            - user solution
-            - solution
-            - keyNumber
-            - fixedString
-            - difficulty
-    */
 
     public function test_verify_captcha_without_token() : void{
         $response = $this->post('api/v1/verify');
