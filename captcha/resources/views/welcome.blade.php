c25db81514e0929743a08c0d158ed7afe98e92fa
gdr-00 <gabriele.dare00@gmail.com>
2023-06-04 15:26:58 +0200

    updated css and blade

--- captcha/resources/views/welcome.blade.php
+++ captcha/resources/views/welcome.blade.php
@@ -28,40 +28,44 @@
             <fieldset class="captcha-images">
                 <legend>Seleziona le immagini appartenente alla classe <component>ClassTarget</component></legend>
                 <div class="img-container">
-                    <input type="checkbox" id="img0" name="" value="0" />
+                    <input type="checkbox" id="img0" name="image[]" value="0" />
                     <label for="img0"><img src=""></label>
                 </div>
                 <div class="img-container">
-                    <input type="checkbox" id="img1" name="" value="1" />
+                    <input type="checkbox" id="img1" name="image[]" value="1" />
                     <label for="img1" ><img src=""></label>
                 </div>
                 <div class="img-container">
-                    <input type="checkbox" id="img2" name="" value="2" />
+                    <input type="checkbox" id="img2" name="image[]" value="2" />
                     <label for="img2"><img src=""></label>
                 </div>
                 <div class="img-container">
-                    <input type="checkbox" id="img3" name="" value="3" />
+                    <input type="checkbox" id="img3" name="image[]" value="3" />
                     <label for="img3"><img src=""></label>
                 </div>
                 <div class="img-container">
-                    <input type="checkbox" id="img4" name="" value="4" />
+                    <input type="checkbox" id="img4" name="image[]" value="4" />
                     <label for="img4"><img src=""></label>
                 </div>
                 <div class="img-container">
-                    <input type="checkbox" id="img5" name="" value="5" />
+                    <input type="checkbox" id="img5" name="image[]" value="5" />
                     <label for="img5"><img src=""></label>
                 </div>
                 <div class="img-container">
-                    <input type="checkbox" id="img6" name="" value="6" />
+                    <input type="checkbox" id="img6" name="image[]" value="6" />
                     <label for="img6"><img src=""></label>
                 </div>
                 <div class="img-container">
-                    <input type="checkbox" id="img7" name="" value="7" />
+                    <input type="checkbox" id="img7" name="image[]" value="7" />
                     <label for="img7"><img src=""></label>
                 </div>
                 <div class="img-container">
-                    <input type="checkbox" id="img8" name="" value="8" />
+                    <input type="checkbox" id="img8" name="image[]" value="8" />
                     <label for="img8"><img src=""></label>
+                </div>
+                <div class="img-container">
+                    <input type="checkbox" id="img9" name="image[]" value="9" />
+                    <label for="img9"><img src=""></label>
                 </div>
             </fieldset>
             <input type="hidden" id="captcha-id" name="captcha-id" value=""/>
