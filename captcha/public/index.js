2c54665ff5db26f236a0b7ace223c7b6f54b1f3c
SinicatoNicola <sinickinfoutili@gmail.com>
2023-06-04 15:03:12 +0200

    insert index.js

---
+++ captcha/public/index.js
@@ -0,0 +1,13 @@
+function Captcha(){
+    let response = fetch('htpps://swe.gdr00.it/api/v1/generate', {
+    method: 'GET',
+    headers: {
+        Authentication: '4|Ag86uaVLYDvQP306TAA0TXawe68LPTkTtVhN8cff'
+    }
+    });
+    let data = response.json();
+    let images_array = [];
+    for(let i = 0; i < 9;i++){
+        images_array.push(data["data"]["captchaImg"]["images"][i]["src"]);
+    }
+}
